248b29231b6125e2b67e1e8115b0f58f7a6a377c
Shiroya Brijesh <brijesh.shiroya.reversebits@gmail.com>
2025-01-24 17:44:14 +0530

    task-10 done..

---
+++ 10_task.js
@@ -0,0 +1,13 @@
+// Return the Remainder from Two Numbers
+function num(a, b){
+    if(a < b){
+        console.log(a);
+    }
+    else if(a == b){
+        console.log(0);
+    }
+    else{
+        console.log(b); 
+    }
+}
+num(35, 12)
